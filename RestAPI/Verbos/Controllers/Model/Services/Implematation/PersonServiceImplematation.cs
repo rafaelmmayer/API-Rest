b4246b72ccf35794cdd5f45bfd69ad7dd97a897b
rafaelmmayer <mayer.rafa@outlook.com>
2021-01-19 21:38:54 -0300

    Inserindo Verbos Http

---
+++ RestAPI/Verbos/Controllers/Model/Services/Implematation/PersonServiceImplematation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Verbos.Controllers.Model.Services.Implematation
+{
+    public class PersonServiceImplematation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long Id)
+        {
+              
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                people.Add(person);
+            }
+            return people;
+        }
+
+        public Person FindById(long Id)
+        {
+            return new Person
+            {
+                Id = IncrementId(),
+                FirstName = "Rafael",
+                LastName = "Mayer",
+                Address = "Vinhedo - SP",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementId(),
+                FirstName = "First Name" + i,
+                LastName = "Last Name" + i,
+                Address = "Address" + i,
+                Gender = "Gender" + i
+            };
+        }
+
+        private long IncrementId()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
---
+++ RestAPI/Verbos/Startup.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Verbos.Controllers.Model.Services;
+using Verbos.Controllers.Model.Services.Implematation;
+
+namespace Verbos
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddScoped<IPersonService, PersonServiceImplematation>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
